15041b6d219b7718ee513079282d1cee88aac2dd
Ranjan Bajracharya <ranjanbajra@gmail.com>
2016-11-14 17:40:59 +0000

    adding comment validation creating method

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,19 @@
 class CommentsController < ApplicationController
-    before_action :is_owner?, only: [:edit, :update, :destroy
-    ]
+     before_action :authenticate_user!, only: :create
+    def create
+     @post = Post.find(params[:post_id])
+     @comment = @post.comments.create(comment_params.merge(user_id: current_user.id))
+        if @comment.valid?
+            redirect_to root_path
+        else
+            flash[:alert] = "Invalid attributes."
+            redirect_to root_path
+        end
+    end
+
+private 
+
+def comment_params
+  params.require(:comment).permit(:text, :post_id)
+end
 end
